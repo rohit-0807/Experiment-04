089d9dcfb135e41469097a8d527cc81c7f48fb14
rohit-0807 <rohitjeerankalagi07@gmail.com>
2025-05-07 12:23:33 +0530

    saved

--- Student.java
+++ Student.java
@@ -12,16 +12,38 @@ public class Student {
 
    void show() {
        System.out.println("Student Name: " + name + "\nPrograme: " + programe);
-       System.out.println("Course List :\n");
+       System.out.println("Course List :");
         for(int i=0;i<course.length;i++){
             System.out.println((i+1)+","+ course[i]);
         }
    }
    
    public static void main(String[] args) {
-        Student s = new Student("Rohit", "BCA", new String[]{"java","python","devops"});
-        s.show();
+        // Student s = new Student("Rohit", "BCA", new String[]{"java","python","devops"});
+        // s.show();
+
+        Course obj1  = new Course("Rohit", "BCA", new String[]{"java","python","devops"},new int[]{30,45,50});
+        obj1.show();
+        obj1.display();
    }
 }
 
+class Course extends Student{
+    
+    int[] marks = new int[3];
 
+   Course(String name, String programe, String[] course,int[] marks){
+    super(name,programe,course);
+    this.marks = marks;
+   }
+
+    void display(){
+        System.out.println("marks less then 40 :");
+        for(int i=0;i<marks.length;i++){
+            if(marks[i] < 40){
+                System.out.println("Course Name: " + course[i] + "\tMarks: " + marks[i]);
+            }
+            
+        }
+    }
+}
